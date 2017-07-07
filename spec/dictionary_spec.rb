70748569368c81739d67d2a7439ed1751a9de204
EdwardMudaida <eddieigo8@gmail.com>
2017-07-07 11:40:50 +0300

    CLear the array of saved words between test runs

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,4 +8,20 @@ describe(Word) do
       expect(Word.all()).to(eq([]))
     end
   end
+
+  describe("#all") do
+    it('add a word to the array of saved words') do
+      my_word = Word.new('Nirvana')
+      my_word.save
+      expect(Word.all()).to(eq([my_word]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out of the saved words') do
+      my_word = Word.new('Nirvana')
+      my_word.save
+      expect(Word.clear()).to(eq([]))
+    end
+  end
 end
