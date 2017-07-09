bec645e6476e0fc8ff3db01a30e18123e84f18fa
EdwardMudaida <eddieigo8@gmail.com>
2017-07-09 22:42:08 +0300

    Add a definition to the selected word

--- app.rb
+++ app.rb
@@ -1,8 +1,43 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/dictionary')
+require('./lib/definition')
+require('./lib/word')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
+end
+
+get('/word/new') do
+  erb(:word_form)
+end
+
+get('/words/:id/definitions/new') do
+  id = params.fetch('id').to_i
+  @word = Word.find(id)
+  erb(:definition_form)
+end
+
+post('/words') do
+  @word = params.fetch('word')
+  word = params.fetch('word')
+  my_word = Word.new(word)
+  my_word.save()
+  erb(:success)
+end
+
+post('/definitions') do
+  @added_word = params.fetch('word')
+  word = params.fetch('word')
+  @definition = Definition.new({word:word})
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i)
+  @word.save_definition(@definition)
+  erb(:word)
+end
+
+get('/words/:id') do
+  @word = Word.find(params.fetch("id"))
+  erb(:word)
 end
